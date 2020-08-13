b4e68a21f06fcfd3773bd1edc8c0d0721da0cf4b
chiedev <dimlaraycille@gmail.com>
2020-08-13 19:32:00 +0800

    [update] TypeScript: basic types

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+
+    /**
+     * Setting up different types.
+     */ 
+    // let firstValue: string = 'Manny';
+    // let firstValue: number = 34;
+    // let firstValue: boolean = true;
+    // let firstValue: number[] = [2, 3, 56];
+    let firstValue: Array<string> = ['2', '3', 'Manny'];
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            The value {firstValue} is of { typeof firstValue} type!
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
