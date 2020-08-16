4a317299fa43d89ecccf2a4e0da0cd5d0c84b174
chiedev <dimlaraycille@gmail.com>
2020-08-16 23:04:16 +0800

    [update] Introduction to interface

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-/**
- * Functional component
- */
-const Message = (prop: {message: string}): any => {
+interface UserMessage {
+    name: string;
+    message: string;
+}
+
+const Message = (props: UserMessage): any => {
     return (
-    <p>{prop.message}</p>
+    <p>{props.name}, {props.message}</p>
     );
 }
 
